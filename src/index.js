ad3563f5bbe6e19a12206c62a17af2f3a5c2e2bd
Joey <legerinvestments@gmail.com>
2021-06-08 23:44:30 -0400

    feat: added buttons and building html with js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,125 @@
 import Isotope from 'isotope-layout';
+import imagesLoaded from 'imagesloaded';
+import images from './assets/images/*.jpg';
 
-return new Isotope('.grid', {
+// TODO:
+// - should move to single row on mobile
+// - links should move to rows of 3 on mobile
+
+const grid = document.querySelector('.grid');
+const list = document.querySelector('.grid-header');
+
+const data = [
+  {
+    name: 'Client One',
+    image: 'portrait',
+    description: 'A simple description of the work',
+    type: 'marketing',
+  },
+  {
+    name: 'Client Two',
+    image: 'square2',
+    description: 'A simple description of the work',
+    type: 'print',
+  },
+  {
+    name: 'Client Three',
+    image: 'square',
+    description: 'A simple description of the work',
+    type: 'mail',
+  },
+  {
+    name: 'Client Four',
+    image: 'portrait',
+    description: 'A simple description of the work',
+    type: 'design',
+  },
+  {
+    name: 'Client Five',
+    image: 'square2',
+    description: 'A simple description of the work',
+    type: 'web',
+  },
+  {
+    name: 'Client Six',
+    image: 'square',
+    description: 'A simple description of the work',
+    type: 'signs',
+  },
+  {
+    name: 'Client Seven',
+    image: 'square',
+    description: 'A simple description of the work',
+    type: 'promo',
+  },
+  {
+    name: 'Client Eight',
+    image: 'square2',
+    description: 'A simple description of the work',
+    type: 'marketing',
+  },
+  {
+    name: 'Client Nine',
+    image: 'portrait2',
+    description: 'A simple description of the work',
+    type: 'print',
+  },
+  {
+    name: 'Client Ten',
+    image: 'square2',
+    description: 'A simple description of the work',
+    type: 'mail',
+  },
+  {
+    name: 'Client Eleven',
+    image: 'square',
+    description: 'A simple description of the work',
+    type: 'design',
+  },
+  {
+    name: 'Client Twelve',
+    image: 'portrait',
+    description: 'A simple description of the work',
+    type: 'web',
+  },
+];
+
+const icons = [
+  { name: 'all', image: '' },
+  { name: 'marketing', image: '' },
+  { name: 'print', image: '' },
+  { name: 'mail', image: '' },
+  { name: 'design', image: '' },
+  { name: 'web', image: '' },
+  { name: 'signs', image: '' },
+  { name: 'promo', image: '' },
+];
+
+const buildContent = data
+  .map(
+    ({ image, type }) => `
+    <div class='grid-item ${type}'>
+      <img class='grid-image' src='${images[image]}' />
+    </div>`
+  )
+  .join('');
+
+const buildIconList = icons
+  .map(
+    ({ name }) => `
+    <li class='list-item'>
+      <button class='list-button' data-filter="${name === 'all' ? '*' : '.' + name}">
+        <div class='list-circle'></div>
+        <div class='list-text'>${name}</div>
+      </button>
+    </li>`
+  )
+  .join('');
+
+grid.insertAdjacentHTML('beforeend', buildContent);
+list.insertAdjacentHTML('beforeend', `<ul>${buildIconList}</ul>`);
+
+const iso = new Isotope(grid, {
   itemSelector: '.grid-item',
   layoutMode: 'masonry',
   masonry: {
@@ -8,9 +127,20 @@ return new Isotope('.grid', {
     gutter: 10,
   },
   percentPosition: !0,
-  sortBy: 'sortnum',
-  getSortData: {
-    category: '[data-category]',
-    sortnum: '[data-num]',
-  },
+});
+
+document.addEventListener('click', function (event) {
+  if (
+    event.target.classList.contains('list-button') ||
+    event.target.parentNode.classList.contains('list-button')
+  ) {
+    const filterValue =
+      event.target.getAttribute('data-filter') ||
+      event.target.parentNode.getAttribute('data-filter');
+    iso.arrange({ filter: filterValue });
+  }
+});
+
+imagesLoaded(grid).on('progress', function () {
+  iso.layout();
 });
